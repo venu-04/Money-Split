f8975a3a229480d1be9d0a5a0564ab74939eccbe
venu-04 <kamsuvenu2004@gmail.com>
2025-09-18 14:35:02 +0530

    deployingg

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -30,7 +30,7 @@ function Login() {
     try {
       setIsSubmitting(true);
       // const res = await axios.post('http://localhost:5000/api/auth/login', {
-      const res = await axios.post('https://money-split.onrender.com/api/auth/login', {
+      const res = await axios.post('https://money-split.onrender.com/api/auth/login', {  
         email,
         password,
       });
