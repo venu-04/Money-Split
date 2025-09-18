e16fe55a0e25d625d8657659b373bcebe3f51dab
venu-04 <kamsuvenu2004@gmail.com>
2025-09-18 14:45:28 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ app.use(cors({
 }))
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/auth',authRoutes);
 app.use('/api/group',groupRoutes);
 app.use('/api/users',userRoutes);
